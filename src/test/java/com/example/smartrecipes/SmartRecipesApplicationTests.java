3dfa8fe6525f7a8bdc545e4707da070907520ca0
art4315 <art4315@gmail.com>
2023-08-14 22:22:29 +0300

    Initial commit

---
+++ src/test/java/com/example/smartrecipes/SmartRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
